6cae4ad34ee5585bb8756f40d1971f00a9780b9f
zhangliang1 <zhangliang5@126.com>
2016-03-29 15:24:30 +0800

    ...

---
+++ src/com/zll/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.zll.demo;
+
+/**
+ * Created by apple on 16/3/29.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
